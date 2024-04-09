ef6179f22b4a3e335b312002f9b8f00114deaf3f
kainholy <ookuraseiya0506@gmail.com>
2024-04-09 11:48:11 +0900

    修正

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import styles from '@/styles/layout/Header.module.scss'
 import Link from 'next/link'
