96928a453cc67452c1604a99dc576043eeea6dbe
kainholy <ookuraseiya0506@gmail.com>
2024-04-09 18:12:11 +0900

    修正

--- src/components/element/HeaderNav.tsx
+++ src/components/element/HeaderNav.tsx
@@ -6,10 +6,10 @@ function HeaderNav({ open }: { open: boolean }) {
   return (
     <nav className={styles.headerNav + ' sp'} aria-hidden={!open}>
         <ul className={styles.list}>
-            <li className={styles.link + ' c-header-link ind'}><Link href="/">トップ</Link></li>
-            <li className={styles.link + ' c-header-link ind'}><Link href="/service">サービス内容</Link></li>
-            <li className={styles.link + ' c-header-link ind'}><Link href="/about">私たちについて</Link></li>
-            <li className={styles.link + ' c-header-link ind'}><Link href="/recruit">採用情報</Link></li>
+            <li className={styles.link + ' c-han-nav-link ind'}><Link href="/">トップ</Link></li>
+            <li className={styles.link + ' c-han-nav-link ind'}><Link href="/service">サービス内容</Link></li>
+            <li className={styles.link + ' c-han-nav-link ind'}><Link href="/about">私たちについて</Link></li>
+            <li className={styles.link + ' c-han-nav-link ind'}><Link href="/recruit">採用情報</Link></li>
         </ul>
     </nav>
   )
