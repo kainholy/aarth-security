786e0fe5a1c8a11383721d3016b1835d4cbe7359
kainholy <ookuraseiya0506@gmail.com>
2024-04-12 16:03:00 +0900

    framer-motion 画面遷移アニメーションのレイアウト崩れ修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,30 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
-import { AnimatePresence } from 'framer-motion'
+import { AnimatePresence } from 'framer-motion';
+import Router from 'next/router';
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps, router }: AppProps) {
+
+  useEffect(() => {
+    const routeChange = () => {
+      const tempFix = () => {
+        const allStyleElements = document.querySelectorAll('link')
+        allStyleElements.forEach((elem) => {
+          if (elem.as === 'style') {
+            elem.rel = 'stylesheet'
+          }
+        })
+      }
+      tempFix()
+    }
+    Router.events.on("routeChangeComplete", routeChange );
+    Router.events.on("routeChangeStart", routeChange );
+    return () => {
+      Router.events.off("routeChangeComplete", routeChange );
+      Router.events.off("routeChangeStart", routeChange );
+    };
+  }, [])
   
   return (
     <AnimatePresence mode="wait" onExitComplete={() => window.scrollTo(0, 0)}>
