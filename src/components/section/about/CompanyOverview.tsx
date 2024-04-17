bdf2653bc75e877529a718427d94adbd43995f0a
kainholy <ookuraseiya0506@gmail.com>
2024-04-17 09:51:32 +0900

    修正

--- src/components/section/about/CompanyOverview.tsx
+++ src/components/section/about/CompanyOverview.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from '@/styles/section/about/CompanyInfo.module.scss'
+import Image from 'next/image'
 
 const data = [
     {
@@ -20,7 +21,7 @@ const data = [
     },
     {
         title: 'TEL',
-        text: '03-5697-9336',
+        text: '03-5697-9333',
     },
     {
         title: 'FAX',
@@ -30,46 +31,55 @@ const data = [
 
 function CompanyInfo() {
   return (
-    <section className={'p-about-company__companyInfo'} id='company-info'>
-        <div className={'p-about-company__container'}>
-            <div className={'p-about-company__titleArea'}>
-                <h2 className={'p-about-company__title c-title3-bold'}>
-                    <span className={'p-about-company__titleEn c-title3-bold-en blue'}>Company Info</span>
-                    <span className={'p-about-company__titleJa c-title3-bold-ja bk'}>会社概要</span>
+    <section className={'p-about-company-info__companyInfo'} id='company-info'>
+        <div className={'p-about-company-info__container'}>
+            <div className={'p-about-company-info__titleArea'}>
+                <h2 className={'p-about-company-info__title c-title3-bold'}>
+                    <span className={'p-about-company-info__titleEn c-title3-bold-en blue'}>Company Info</span>
+                    <span className={'p-about-company-info__titleJa c-title3-bold-ja bk'}>会社概要</span>
                 </h2>
             </div>
-            <div className={'p-about-company__content'}>
-                <table className={'p-about-company__table'}>
+            <div className={'p-about-company-info__content'}>
+                <table className={'p-about-company-info__table'}>
                     {
                         data.map((item, index) => (
-                            <tr key={index} className={'p-about-company__tr'}>
-                                <th className={'p-about-company__th c-about-company-info-title'}>{item.title}</th>
-                                <td className={'p-about-company__td c-about-company-info-text'}>{item.text}</td>
+                            <tr key={index} className={'p-about-company-info__tr'}>
+                                <th className={'p-about-company-info__th c-about-company-info-title'}>{item.title}</th>
+                                <td className={'p-about-company-info__td c-about-company-info-text'}>{item.text}</td>
                             </tr>
                         ))
                     }
-                    <tr className={'p-about-company__tr'}>
-                        <th className={'p-about-company__th c-about-company-info-title'}>グループ事業</th>
-                        <td className={'p-about-company__td c-about-company-info-text'}>
-                            <p className={'p-about-company__name'}>株式会社アール・ウェイブ</p>
-                            <p className={'p-about-company__department'}>洗車・コーティング部門</p>
-                            <p className={'p-about-company__department'}>モーターサイクル部門</p>
-                            <p className={'p-about-company__department'}>警備部門</p>
-                            <p className={'p-about-company__department'}>飲食店部門</p>
-                            <p className={'p-about-company__name'}>ウィンズ株式会社</p>
-                            <p className={'p-about-company__department'}>車両部門</p>
-                            <p className={'p-about-company__name'}>株式会社アース</p>
-                            <p className={'p-about-company__department'}>不動産部門</p>
+                    <tr className={'p-about-company-info__tr'}>
+                        <th className={'p-about-company-info__th c-about-company-info-title'}>グループ事業</th>
+                        <td className={'p-about-company-info__td c-about-company-info-text'}>
+                            <p className={'p-about-company-info__name'}>株式会社アール・ウェイブ</p>
+                            <p className={'p-about-company-info__department'}>洗車・コーティング部門</p>
+                            <p className={'p-about-company-info__department'}>モーターサイクル部門</p>
+                            <p className={'p-about-company-info__department'}>警備部門</p>
+                            <p className={'p-about-company-info__department'}>飲食店部門</p>
+                            <p className={'p-about-company-info__name'}>ウィンズ株式会社</p>
+                            <p className={'p-about-company-info__department'}>車両部門</p>
+                            <p className={'p-about-company-info__name'}>株式会社アース</p>
+                            <p className={'p-about-company-info__department'}>不動産部門</p>
                         </td>
                     </tr>
-                    <tr className={'p-about-company__tr'}>
-                        <th className={'p-about-company__th c-about-company-info-title'}>警備事業内容</th>
-                        <td className={'p-about-company__td c-about-company-info-text'}>
+                    <tr className={'p-about-company-info__tr'}>
+                        <th className={'p-about-company-info__th c-about-company-info-title'}>警備事業内容</th>
+                        <td className={'p-about-company-info__td c-about-company-info-text'}>
                             <p>雑踏(イベント)警備、交通誘導警備、駐車場警備</p>
-                            <p className={'p-about-company__caption'}>※詳細は「サービス内容」ページをご覧ください。</p>
+                            <p className={'p-about-company-info__caption'}>※詳細は「サービス内容」ページをご覧ください。</p>
                         </td>
                     </tr>
                 </table>
+            </div>
+            <div className={'p-about-company-info__imgArea'}>
+                <Image
+                    src='/about/company-info/1.webp'
+                    alt='堂々たる技術、誠実なサービス'
+                    className={'p-about-company-info__img'}
+                    width={1000}
+                    height={1000}
+                />
             </div>
         </div>
     </section>
