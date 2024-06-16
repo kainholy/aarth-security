4f3f6eef5b16b7db6df6b3a6ad0764b69a748563
kainholy <ookuraseiya0506@gmail.com>
2024-06-17 08:49:15 +0900

    Instagram追加

--- src/components/element/HeaderButton.tsx
+++ src/components/element/HeaderButton.tsx
@@ -4,8 +4,8 @@ function HeaderButton({ open, onClick }: { open: boolean, onClick: () => void}){
 
   return (
     <button className={'p-element-header-button__headerButton sp'} onClick={onClick} aria-expanded={open}>
-        <span className={'p-element-header-button__line1'}></span>
-        <span className={'p-element-header-button__line2'}></span>
+        <span className={'p-element-header-button__line1 line1'}></span>
+        <span className={'p-element-header-button__line2 line2'}></span>
     </button>
   )
 }
